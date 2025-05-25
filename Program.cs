3ee2bb9cf67e63918b7694cbe617975b9e78c0ca
Kwazi-code <lehlohonolomashaya@gmail.com>
2025-05-25 18:54:33 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void CountItems<T>(List<T> items)
+    {
+        Dictionary<T, int> counts = new Dictionary<T, int>();
+
+        foreach (T item in items)
+        {
+            if (counts.ContainsKey(item))
+            {
+                counts[item] = counts[item] + 1;
+            }
+            else
+            {
+                counts[item] = 1;
+            }
+        }
+
+        foreach (var pair in counts)
+        {
+            Console.WriteLine(pair.Key + ": " + pair.Value);
+        }
+    }
+
+    static void Main()
+    {
+        var list1 = new List<string> { "apple", "banana", "apple", "orange", "banana", "apple" };
+        var list2 = new List<int> { 1, 2, 2, 3, 1, 4, 2 };
+
+        Console.WriteLine("String list:");
+        CountItems(list1);
+
+        Console.WriteLine("\nInteger list:");
+        CountItems(list2);
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# CountItemOccurrences
+
+This is a C# console application that counts how many times each item appears in a list. The function is generic and works with any data type, including strings, integers, and characters.
+
+## Features
+
+- Accepts a list of any data type using generics.
+- Outputs the number of times each item appears in the list.
+- Demonstrates the use of dictionaries and basic control flow.
+
+
+
